1afb95022b9616bdd6971ab3f3a1a7582b9cd6a0
Krixx <dev@krixx.anonaddy.com>
2026-03-31 22:28:17 +0200

    fixing harden imports part 1

--- cmake/SyncRedist.cmake
+++ cmake/SyncRedist.cmake
@@ -8,7 +8,34 @@ endif()
 
 file(MAKE_DIRECTORY "${BURNER_REDIST_DIR}")
 
-file(GLOB burner_runtime_dlls "${BURNER_SOURCE_DIR}/*.dll")
+if(NOT DEFINED BURNER_COPY_TO_SOURCE_DIR)
+    set(BURNER_COPY_TO_SOURCE_DIR ON)
+endif()
+
+set(burner_runtime_dlls)
+
+if(DEFINED BURNER_RUNTIME_DLL_FILES AND NOT BURNER_RUNTIME_DLL_FILES STREQUAL "")
+    list(APPEND burner_runtime_dlls ${BURNER_RUNTIME_DLL_FILES})
+endif()
+
+if(DEFINED BURNER_RUNTIME_DLL_DIRS AND NOT BURNER_RUNTIME_DLL_DIRS STREQUAL "")
+    foreach(runtime_dir IN LISTS BURNER_RUNTIME_DLL_DIRS)
+        if(EXISTS "${runtime_dir}")
+            file(GLOB runtime_dir_dlls "${runtime_dir}/*.dll")
+            list(APPEND burner_runtime_dlls ${runtime_dir_dlls})
+        endif()
+    endforeach()
+endif()
+
+file(GLOB burner_source_dir_dlls "${BURNER_SOURCE_DIR}/*.dll")
+list(APPEND burner_runtime_dlls ${burner_source_dir_dlls})
+list(REMOVE_DUPLICATES burner_runtime_dlls)
+
 foreach(runtime_dll IN LISTS burner_runtime_dlls)
-    file(COPY "${runtime_dll}" DESTINATION "${BURNER_REDIST_DIR}")
+    if(EXISTS "${runtime_dll}")
+        if(BURNER_COPY_TO_SOURCE_DIR)
+            file(COPY "${runtime_dll}" DESTINATION "${BURNER_SOURCE_DIR}")
+        endif()
+        file(COPY "${runtime_dll}" DESTINATION "${BURNER_REDIST_DIR}")
+    endif()
 endforeach()
